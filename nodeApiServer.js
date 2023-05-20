93308e497d392d197b51e7bc7696be671bd67ca2
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-05-20 01:23:18 -0700

    Update nodeApiServer.js

--- nodeApiServer.js
+++ nodeApiServer.js
@@ -97,7 +97,7 @@ app.post('/api', (req, res) => {
 });
 
 // Start the server
-const port = 1251;
+const port = 443;
 app.listen(port, () => {
 	console.log(`API server running on port ${port}`);
 });
